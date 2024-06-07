76fde49de5dc943b91dbce2ab8fd15d0dd072e02
Bazalter <egor-zaikin3@yandex.ru>
2024-06-07 11:14:34 +0300

    3 tasks has been solved from second lessons

--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный линейный поиск/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный линейный поиск/task.py
@@ -11,4 +11,19 @@ def min_search(arr: List[int]) -> int:
     :param arr: Массив целых чисел
     :return: Индекс первого вхождения элемента в массиве
     """
-    ...  # TODO реализовать итеративный линейный поиск
+    if not arr:
+        raise ValueError()
+    a = None
+    b = None
+    for i, j in enumerate(arr):
+        if a is None:
+            a = i
+            b = j
+        if b > j:
+            a = i
+            b = j
+    return a
+
+
+print(min_search([-10, -9, 0]))
+
--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный факториал/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный факториал/task.py
@@ -5,4 +5,15 @@ def factorial_iterative(n: int) -> int:
     :param n: Число, факториал которого нужно найти
     :return: n! - факториал числа n
     """
-    ...  # TODO реализовать итеративный алгоритм нахождения факториала
+    if n < 0:
+        raise ValueError()
+    res = 1
+    for i in range(n+1):
+        if i == 0:
+            res = 1
+        else:
+            res *= i
+
+    return res
+
+# print(factorial_iterative(5))
