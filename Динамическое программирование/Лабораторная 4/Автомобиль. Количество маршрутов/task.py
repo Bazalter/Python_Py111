2ed63df3d656db7a6482e50b27c8342509fc41e7
Bazalter <egor-zaikin3@yandex.ru>
2024-06-22 20:14:43 +0300

    lab 4 complete 22.06.24

--- Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
+++ Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import pprint
 
 def car_paths(n: int, m: int) -> List[List[int]]:
     """
@@ -11,6 +11,27 @@ def car_paths(n: int, m: int) -> List[List[int]]:
     :return: Новую таблицу с посчитанным количеством маршрутов в каждую клетку
     """
     ...  # TODO решить задачу с помощью динамического программирования
+
+    result = [[0]*n for i in range(m)]
+    # pprint.pprint(result, width=20)
+
+    for i in range(n):
+        result[0][i] = 1
+
+    # pprint.pprint(result, width=20)
+
+    for i in range(1, m):
+        result[i][0] = 1
+
+    # pprint.pprint(result, width=20)
+
+    for i in range(1, m):
+        for j in range(1, n):
+            result[i][j] = result[i-1][j] + result[i][j-1] + result[i-1][j-1]
+
+    pprint.pprint(result, width=20)
+    return result
+
 
 
 if __name__ == '__main__':
--- Динамическое программирование/Самостоятельная работа/Ход коня. Ленивая динамика/task.py
+++ Динамическое программирование/Самостоятельная работа/Ход коня. Ленивая динамика/task.py
@@ -11,6 +11,28 @@ def calculate_paths(shape: Tuple[int, int]) -> int:
     """
     ... # TODO реализуйте подсчет ходов коня
 
+    moves = [(2, 1), (2, -1), (1, 2), (-1, 2)]
+
+    @lru_cache
+    def count_path(x, y):
+        if x < 0 or x >= shape[0] or y < 0 or y >= shape[1]:
+            return 0
+        if x == 0 and y == 0:
+            return 1
+        count = 0
+        for move in moves:
+            prev_x = x - move[0]
+            prev_y = y - move[1]
+            count += count_path(prev_x, prev_y)
+
+        return count
+
+    return count_path(shape[0]-1, shape[1]-1)
+
+
+
+
+
 
 if __name__ == '__main__':
     print(calculate_paths((4, 4)))  # 2
