6a653433a5ddd3f8a72221cf2364bc5bad0d0c2a
Bazalter <egor-zaikin3@yandex.ru>
2024-06-25 22:29:40 +0300

    lab 4 complete 4 task 25.06

--- Динамическое программирование/Самостоятельная работа/Треугольник паскаля/task.py
+++ Динамическое программирование/Самостоятельная работа/Треугольник паскаля/task.py
@@ -2,4 +2,23 @@ from typing import List
 
 
 def generate(num_rows: int) -> List[List[int]]:
-    ...
+
+    if num_rows == 0:
+        return []
+
+    if num_rows == 1:
+        return [[1]]
+
+    list_pasc = [[1]]
+
+    for i in range(1, num_rows):
+        row = [1]
+        for j in range(1, i):
+            row.append(list_pasc[i-1][j-1] + list_pasc[i-1][j])
+        row.append(1)
+        list_pasc.append(row)
+    return list_pasc
+
+
+
+
